3a6d57cf843f5b4484e56598bf52ab6484e905b8
zhiqiangz <hyw511322@gmail.com>
2025-02-27 14:16:57 +0800

    Update reserve conda env path

--- apps_config/tmux/scripts/reserve_conda_env.sh
+++ apps_config/tmux/scripts/reserve_conda_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Step 1: Read ~/.tmux/resurrect/last and process each line that starts with 'pane'
 config_home=$HOME/.tmux/resurrect
-last_file=$(readlink "$config_home/last")
+last_file=$config_home/$(readlink "$config_home/last")
 
 reserve_file="${last_file/tmux_resurrect/conda_env}"
 # Ensure reserve file exists (it will be created if it doesn't)
@@ -12,9 +12,6 @@ else
 fi
 
 reserve_file_sl="$config_home/conda_env_reservation.txt"
-if [ -f "$reserve_file" ]; then
-    rm -f "$reserve_file_sl"
-fi
 ln -sf "$reserve_file" "$reserve_file_sl"
 
 # Step 2: Parse each line that starts with "pane"
@@ -24,5 +21,5 @@ grep "^pane" "$last_file" | while read -r line; do
  
      # # Step 4: Save the conda environment to the reserve file (with session, window, and pane info)
      tmux send-keys -t "${session_name}":"${window_index}"."${pane_index}" \
-         "echo ${session_name}:${window_index}.${pane_index} \$CONDA_DEFAULT_ENV >>$reserve_file_sl" C-m
+         " echo ${session_name}:${window_index}.${pane_index} \$CONDA_DEFAULT_ENV >>$reserve_file_sl" C-m
 done
